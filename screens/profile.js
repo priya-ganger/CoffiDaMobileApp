8b9ea6288c8561ad6d97e41d73bd86c340e82c5a
Priya Ganger <priya.ganger@hotmail.com>
2021-02-10 01:16:23 +0000

    feat(trying to implement post endpoint for user data)

--- screens/profile.js
+++ screens/profile.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import { View, StyleSheet, Text, ActivityIndicator, FlatList, ToastAndroid, Alert } from "react-native";
+import { View, StyleSheet, Text, ActivityIndicator, FlatList, ToastAndroid, Alert, TextInput, Button } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import Location from '../components/location';
 
 class Profile extends Component {
 
@@ -10,7 +11,19 @@ class Profile extends Component {
 
       this.state = {
           isLoading: true,
-          userData: []
+          userData: [],
+
+           currentFirstName: 'h',
+           currentLastName: 'g',
+           currentEmail: 'h@mmu.ac.uk',
+           currentPassword: 'hello12345',
+
+          first_name: '',
+          last_name: '',
+          email: '',
+          password: '',
+
+         
   };
 }
 
@@ -58,9 +71,58 @@ getUserData = async () => {
   });
 }
 
+updateUserInfo(){
+  let sendData = {};
+
+  if(this.state.first_name != this.state.currentFirstName){
+    sendData['first_name'] = this.state.first_name;
+
+  }
+
+  if(this.state.last_name != this.state.currentLastName){
+    sendData['last_name'] = this.state.last_name;
+
+  }
+
+  if(this.state.email != this.state.currentEmail){
+    sendData['email'] = this.state.email;
+
+  }
+
+  if(this.state.password != this.state.currentPassword){
+    sendData['password'] = this.state.password;
+
+  }
+
+  console.log(sendData);
+    
+
+  const token = AsyncStorage.getItem('session_token');
+  const userId = AsyncStorage.getItem('user_id');
+  console.log("Trying to get user data")
+  return fetch('http://10.0.2.2:3333/api/1.0.0/user/'+userId, {
+    method: 'patch',
+    headers: {
+        'Content-Type': 'application/json',
+        'X-Authorization': token
+
+    },
+    body: JSON.stringify(sendData)
+
+})
+.then((response) => {
+    Alert.alert("User info updated");
+})
+.catch((error) => {
+    console.log(error);
+    ToastAndroid.show(error, ToastAndroid.SHORT);
+})
+}
+
   render() {
         const navigation = this.props.navigation;
         const item = this.state.userData;
+       
        
         if(this.state.isLoading){
           return(
@@ -75,7 +137,40 @@ getUserData = async () => {
            <Text>User First Name:  {item.first_name}</Text>
             <Text>User Last Name: {item.last_name}</Text>
             <Text>User Email: {item.email}</Text>
-            <Text>Password: {item.password}</Text>
+            <Text>Password:</Text>
+
+            <TextInput
+                placeholder={'Enter your first name'}
+                onChangeText={(first_name)=>this.setState({first_name})}
+                value={this.state.first_name}
+             />
+
+             <TextInput
+                placeholder="Enter your last name"
+                onChangeText={(last_name)=>this.setState({last_name})}
+                value={this.state.last_name}
+               
+              />
+            
+            <TextInput 
+                placeholder="Email"
+                onChangeText={(email)=>this.setState({email})}
+                value={this.state.email}
+             />
+
+             <TextInput 
+                placeholder="Password"
+                onChangeText={(password) => this.setState({password})}
+                value={this.state.password}
+                secureTextEntry={true}
+             />
+
+                <Button 
+                title="Update info"
+                onPress={() => this.updateUserInfo()}
+                ></Button>
+
+
       </View>
 
      )
