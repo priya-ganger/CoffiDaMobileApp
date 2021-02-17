9f16e98cf98973573ec170fa3ca4f741652d3997
Priya Ganger <priya.ganger@hotmail.com>
2021-02-17 00:41:38 +0000

    feat(search): added logic to filter by ratings

--- screens/search.js
+++ screens/search.js
@@ -1,32 +1,33 @@
 import { NavigationContainer } from '@react-navigation/native';
 import React, { Component } from 'react';
-import { Text, View, Image, Button, FlatList, ActivityIndicator, TextInput } from 'react-native';
-//import { Rating, AirbnbRating } from 'react-native-elements';
+import { Text, View, Image, Button, FlatList, ActivityIndicator, TextInput, StyleSheet  } from 'react-native';
+import { AirbnbRating } from 'react-native-ratings';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 class Search extends Component{
 
-    constructor(props)
-  {
+    constructor(props){
       super(props);
 
       this.state = {
           isLoading: true,
           locationData: null,
           q: '',
-          overall_rating: 0
-
-  };
+          overall_rating: 0,
+          price_rating: 0,
+          quality_rating: 0,
+          clenliness_rating: 0
+  }
 }
 
 componentDidMount() {
-    this.getLocationData('http://10.0.2.2:3333/api/1.0.0/find');
+    this.getLocationData("http://10.0.2.2:3333/api/1.0.0/find");
   }
 
-  getLocationData = async () => {
+  getLocationData = async (url) => {
     const value = await AsyncStorage.getItem('session_token');
     console.log("Trying to get data for search")
-    return fetch('http://10.0.2.2:3333/api/1.0.0/find',{
+    return fetch(url,{
       'headers': {
         'X-Authorization': value
       }
@@ -63,11 +64,13 @@ componentDidMount() {
   }
 
   search =() => {
-    let url = ''
+    let url = 'http://10.0.2.2:3333/api/1.0.0/find?'
 
     console.log(this.state.q);
     console.log(this.state.overall_rating);
     console.log(this.state.price_rating);
+    console.log(this.state.quality_rating);
+    console.log(this.state.clenliness_rating);
 
     if(this.state.q != ''){
         url += "q=" + this.state.q + "&";
@@ -77,21 +80,33 @@ componentDidMount() {
         url += "overall_rating=" + this.state.overall_rating + "&";
     }
 
+    if(this.state.price_rating > 0){
+      url += "price_rating=" + this.state.price_rating + "&";
+     }
+
+  if(this.state.quality_rating > 0){
+    url += "quality_rating=" + this.state.quality_rating + "&";
+    }
+
+if(this.state.clenliness_rating > 0){
+  url += "clenliness_rating=" + this.state.clenliness_rating + "&";
+}
+
     this.getLocationData(url);
   }
 
+  ratingCompleted(rating, name) {
+    let stateObject = () => {
+      let returnObj = {};
+      returnObj[name] = rating;
+      return returnObj
+    };
+    this.setState( stateObject );
+
+    //to avoid 'name' being updated
+  }
+
     render(){
-        
-        const navigation = this.props.navigation;
-
-        // return(
-        //     <View>
-        //       <Text>This is the Search screen</Text>
-        //       <Button title="Go back"
-        //         onPress={() => navigation.goBack()} />  
-        //     </View>
-        // )
-
         if(this.state.isLoading){
             return(
             <View>
@@ -101,34 +116,72 @@ componentDidMount() {
         }else{
         return (
             <View>
-              <Text>Search!</Text>
+              <Text>Search a location:</Text>
               <TextInput
-                placeholder={'Search here'}
+                placeholder={'Type here'}
                 onChangeText={(q)=>this.setState({q: q})}
                 value={this.state.q}
              />
+
+             <Text>Overall Rating</Text>
+             <AirbnbRating
+             size={15}
+             defaultRating={0}
+             reviewSize={20}
+             selectedColor={'#f1c40f'}
+             unSelectedColor={'black'}
+             reviews={['Terrible', 'Bad', 'Average', 'Good', 'Great']}
+             onFinishRating={(rating) => this.ratingCompleted(rating, "overall_rating")}
+             />
+
+            {/* <Text>Price Rating</Text>
+             <AirbnbRating
+             size={15}
+             defaultRating={0}
+             selectedColor={'#f1c40f'}
+             unSelectedColor={'black'}
+             reviews={['Terrible', 'Bad', 'Average', 'Good', 'Great']}
+             onFinishRating={(rating) => this.ratingCompleted(rating, "price_rating")}
+             />
+
+            <Text>Quality Rating</Text>
+             <AirbnbRating
+             size={15}
+             defaultRating={0}
+             selectedColor={'#f1c40f'}
+             unSelectedColor={'black'}
+             reviews={['Terrible', 'Bad', 'Average', 'Good', 'Great']}
+             onFinishRating={(rating) => this.ratingCompleted(rating, "quality_rating")}
+             />
+
+            <Text>Cleanliness Rating</Text>
+             <AirbnbRating
+             size={15}
+             defaultRating={0}
+             selectedColor={'#f1c40f'}
+             unSelectedColor={'black'}
+             reviews={['Terrible', 'Bad', 'Average', 'Good', 'Great']}
+             onFinishRating={(rating) => this.ratingCompleted(rating, "clenliness_rating")}
+             /> */}
 
              <Button
             title="Search"
             onPress={() =>{this.search()}}
              />
 
-<FlatList
-data={this.state.locationData}
-renderItem={({item}) => (
+        <FlatList
+        data={this.state.locationData}
+        renderItem={({item}) => (
   
     <View>
-        <Text>Location ID:{item.location}</Text> 
+        <Text>Location ID:{item.location_id}</Text> 
         <Text> Name:  {item.location_name}</Text>
         <Text> Town: {item.location_town}</Text>
-        {/* <Text>Location Latitude: {item.latitude}</Text>
-        <Text>Location Longitude:  {item.longitude}</Text> */}
         {/* <Text>Location Photo   {item.photo_path}</Text> */}
          {/* {this.state.displayImg ? ( 
           */}
           <Image 
            source={{uri: item.photo_path}}
-          //source={{uri: 'https://tr-images.condecdn.net/image/vOkb7Jmdv2L/crop/1020/f/1kaffeine-london-mar19-pr.jpg'}}
           style={{width: 400, height: 400}}
           onError={this.errorLoadingImg}
           />
@@ -141,13 +194,13 @@ renderItem={({item}) => (
         <Text> Cleanliness Rating: {item.avg_clenliness_rating}</Text>
         <Text>  </Text>
         </View>
-)}
-keyExtractor={(item,index) => item.location_id.toString()}
-/>
-</View>
- )      
+          )}
+          keyExtractor={(item,index) => item.location_id.toString()}
+          />
+          </View>
+          )      
         }
-
     }
 }
+
 export default Search;
