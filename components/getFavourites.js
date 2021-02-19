95ebc36876ecff0d0899cd318b87bd0aad29b697
Priya Ganger <priya.ganger@hotmail.com>
2021-02-19 01:54:33 +0000

    feat(favourites): added lists to view user's reviews and liked reviews

--- components/getFavourites.js
+++ components/getFavourites.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, ActivityIndicator, ToastAndroid, Alert, FlatList, Image } from 'react-native';
+import { Text, Button, View, ActivityIndicator,ScrollView, ToastAndroid, Alert, FlatList, Image } from 'react-native';
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 class GetFavourites extends Component{
@@ -68,45 +68,66 @@ class GetFavourites extends Component{
       );
       }else{
       return (
-          <View>
-            <FlatList
+          <ScrollView>
+             <FlatList
             
                 data={this.state.userData.favourite_locations}
                 renderItem={({item}) => (
                     <View>
-                        <Text>Location ID:{item.location_id}</Text>
+                        {/* <Text>Location ID:{item.location_id}</Text> */}
                         <Text>Location Name:  {item.location_name}</Text>
                         <Text>Location Town: {item.location_town}</Text>
-                        <Text>Location Latitude: {item.latitude}</Text>
-                        <Text>Location Longitude:  {item.longitude}</Text>
-                        <Text>Location Photo   {item.photo_path}</Text>
                          {/* {this.state.displayImg ? ( 
                           */}
-                          <Image 
+                           <Image 
                            source={{uri: item.photo_path}}
                           //source={{uri: 'https://tr-images.condecdn.net/image/vOkb7Jmdv2L/crop/1020/f/1kaffeine-london-mar19-pr.jpg'}}
                           style={{width: 200, height: 200}}
                           onError={this.errorLoadingImg}
-                          />
-              
-                        {/* //   ) : (
-                        //    <View></View>
-                        //  )} */}
-                        <Text>Location Average Overall Rating:{item.avg_overall_rating}</Text>
-                        <Text>Location Price Rating:{item.avg_price_rating}</Text>
-                        <Text>Location Quality Rating:{item.avg_quality_rating}</Text>
-                        <Text>Location Cleanliness Rating:{item.avg_clenliness_rating}</Text>
-                        <Text>  </Text>
-                       
+                          /> 
+              <Button 
+                  title="More info"
+                  onPress={() => this.props.navigation.navigate('moreInfo', {locationID: item.location_id})}
+                ></Button>
                     </View>
                 )}
                 keyExtractor={(item,index) => item.location_id.toString()}
-                />
+                /> 
 
+ <Text>Your Reviews</Text>
+        <FlatList
+            data={this.state.userData.reviews}
+            renderItem={({item}) => (
+                <View>
+                    <Text>Location Name: {item.location.location_name}</Text>
+                    <Text>Review {item.review.review_body}</Text>
+                    <Text>Overall rating: {item.review.overall_rating}  </Text>
+                    <Text>Price Rating: {item.review.price_rating} </Text>
+                    <Text>Quality Rating: {item.review.quality_rating} </Text>
+                    <Text>Cleanliness Rating: {item.review.clenliness_rating} </Text>
+                    <Text> </Text>
+                </View>
+            )}
+            keyExtractor={(item,index) => item.review.review_id.toString()}
+            />
 
-
-
-      </View>
+<Text>Your liked Reviews</Text>
+        <FlatList
+            data={this.state.userData.liked_reviews}
+            renderItem={({item}) => (
+                <View>
+                    <Text>Location Name: {item.location.location_name}</Text>
+                    <Text>Review {item.review.review_body}</Text>
+                    {/* <Text>Overall rating: {item.review.overall_rating}  </Text>
+                    <Text>Price Rating: {item.review.price_rating} </Text>
+                    <Text>Quality Rating: {item.review.quality_rating} </Text>
+                    <Text>Cleanliness Rating: {item.review.clenliness_rating} </Text> */}
+                    <Text> </Text>
+                </View>
+            )}
+            keyExtractor={(item,index) => item.review.review_id.toString()}
+            />
+      </ScrollView>
 
             )
         }
