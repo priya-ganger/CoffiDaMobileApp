8c7d252f0f30ffd5ca00cfa2583a51be8ece5d2f
Priya Ganger <priya.ganger@hotmail.com>
2021-03-03 01:48:20 +0000

    feat(trying to work out distance for locations)

--- screens/settings.js
+++ screens/settings.js
@@ -3,13 +3,14 @@ import {  TextInput, FlatList, SafeAreaView, View, TouchableOpacity, Text, Alert
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
 import Geolocation from 'react-native-geolocation-service';
+import { getDistance } from 'geolib';
 
 async function requestLocationPermission() {
   try{
     const granted = await PermissionsAndroid.request(
       PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
       {
-        title: 'Lab04 Location Permission',
+        title: ' Location Permission',
         message: 'This app requires access to your location',
         buttonNeutral: 'Ask me later',
         buttonNegative: 'Cancel',
@@ -41,23 +42,67 @@ class Settings extends Component {
      locationData: [],
       location_id: '',
       longitude: '',
-      latitude: ''
+      latitude: '',
+
+      //test
+      locationsLong: '',
+      locationsLat: ''
     }
+  }
+
+  componentDidMount(){
+    this.findCoordinates()
+    this.getLocationData()
+    this.calculateDistance();
   }
 
   findCoordinates = () => {
     if(!this.state.locationPermission){
       this.state.locationPermission = requestLocationPermission();
     }
+    Geolocation.getCurrentPosition(
+      (position) => {
+       // const location = JSON.stringify(position);
 
-    this.setState({
-      location: {
-        longitude: -2.2511616,
-        latitude: 53.510144
+        const location = position;
+        console.log("Location1 ", location.coords);
+
+        this.setState({ location: {
+          longitude: location.coords.longitude,
+          latitude: location.coords.latitude
+        },
+      
+      });
+      this.setState({isLoading: false});
+      this.setState({longitude: location.coords.longitude});
+      this.setState({latitude: location.coords.latitude});
+    
+      console.log("longitude ", location.coords.longitude);
+      console.log("latitude ", location.coords.latitude);
+
+      console.log("longitude2 ", this.state.longitude);
+      console.log("latitude2 ", this.state.latitude);
+    },
+      (error) => {
+        Alert.alert(error.message)
       },
-      isLoading: false
-    })
-  }
+      {
+        enableHighAccuracy: true,
+        timeout: 15000,
+        maximumAge: 10000 
+      }
+    );
+  };
+  
+
+  //   this.setState({
+  //     location: {
+  //       longitude: -2.2511616,
+  //       latitude: 53.510144
+  //     },
+  //     isLoading: false
+  //   })
+  // }
 
   getLocationData = async () => {
     const value = await AsyncStorage.getItem('session_token')
@@ -90,34 +135,25 @@ class Settings extends Component {
       })
   }
 
-  //   Geolocation.getCurrentPosition(
-  //     (position) => {
-  //      // const location = JSON.stringify(position);
+   
+      
+    
 
-  //       const location = position;
-  //       console.log("Location1", location.coords);
+   calculateDistance = () => {
 
-  //       this.setState({ location: {
-  //         longitude: location.coords.longitude,
-  //         latitude: location.coords.latitude
-  //       }});
-  //     this.setState({isLoading: false});
-  //   },
-  //     (error) => {
-  //       Alert.alert(error.message)
-  //     },
-  //     {
-  //       enableHighAccuracy: true,
-  //       timeout: 15000,
-  //       maximumAge: 10000 
-  //     }
-  //   );
-  // };
+    
+    console.log("tesing: " + this.state.locationData)
 
-  componentDidMount(){
-    this.findCoordinates();
-    this.getLocationData();
-  }
+    var dis = getDistance(
+      {latitude: this.state.latitude, longitude: this.state.longitude},
+      {latitude: 51.528308, longitude: -0.3817765},
+    );
+    alert(
+      `Distance\n\n${dis} Meter\nOR\n${dis / 1000} KM + ${this.state.locationData}`
+    );
+  };
+
+
 
   render() {
     if(this.state.isLoading){
@@ -128,18 +164,14 @@ class Settings extends Component {
       )
     }else{
       console.log("location 2: ", this.state.location);
+      console.log("location 2: ", this.state.latitude);
+      console.log("location 2: ", this.state.longitude);
       return (
         <View style={{flex:1}}>
-        {/* <Text>This is the Settings screen</Text>
-        <Button
-          title='Go back'
-          onPress={() => navigation.goBack()}
-        /> */}
-    
-        <MapView
-        provider={PROVIDER_GOOGLE}
-       style={{flex: 0.5}}
-        region={{
+        {/* <MapView
+          provider={PROVIDER_GOOGLE}
+          style={{flex: 0.5}}
+          region={{
           latitude: this.state.location.latitude,
           longitude: this.state.location.longitude,
           latitudeDelta: 1.000,
@@ -151,7 +183,7 @@ class Settings extends Component {
           title="My location"
           description="Hi, here I am"
           />
-        </MapView>
+        </MapView> */}
         {/* <Text>Hello</Text> */}
         <View>
         <FlatList
@@ -160,7 +192,18 @@ class Settings extends Component {
               <View>
                 <Text>Name: {item.location_name}</Text>
                 <Text> latitude:  {item.latitude}</Text>
-                <Text> longitude: {item.longitude}</Text>
+                <Text> longitude: {item.longitude} </Text>
+
+                {/* {this.setState ({ locationsLat:
+                  item.longitude})}
+
+                {this.setState ({ locationsLong:
+                  item.longitude})}   */}
+
+
+
+
+                <Text>Distance:  </Text>
               </View>
             )}
             keyExtractor={(item, index) => item.location_id.toString()}
@@ -168,12 +211,6 @@ class Settings extends Component {
           </View>
          </View>
       );
-
-     /* <Button
-    //   title='Get my coordinates'
-    //   onPress={() => {this.findCoordinates()}}
-    // /> 
-    // <Text>Location: {this.state.location}</Text> */
 
 }
 }
